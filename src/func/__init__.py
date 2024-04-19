c1d04e149b63584644f0e4011a8974d2618b4a78
kritserv <krittipoom@outlook.com>
2024-04-19 13:00:11 +0700

    rename event_checker to pygame_event_checker

--- src/func/__init__.py
+++ src/func/__init__.py
@@ -1,6 +1,6 @@
 from .debug import print_debug
 from .drawer import blit_text
-from .event_checker import check_event
+from .pygame_event_checker import check_event
 from .load_screen_data import load_screen_from_json
 from .resize_screen import toggle_full_screen, update_size
 from .load_scene_data import load_scene_from_json
