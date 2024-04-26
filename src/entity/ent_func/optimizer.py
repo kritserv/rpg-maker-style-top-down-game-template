6658fc3b797e9408467d0c1d389d67d5bf630d35
kritserv <krittipoom@outlook.com>
2024-04-26 12:38:53 +0700

    clean cache if len(cache_dict) >= 20

--- src/entity/ent_func/optimizer.py
+++ src/entity/ent_func/optimizer.py
@@ -5,7 +5,14 @@ def data_is_in_cache(cache_dict, data_key):
 	except KeyError:
 		return False
 
+def clear_cache(cache_dict):
+	cache_dict = {}
+
 def add_data_to_cache(cache_dict, data_key, data):
+	
+	if len(cache_dict) >= 20:
+		clear_cache(cache_dict)
+
 	cache_dict[data_key] = data
 
 def load_data_from_cache(cache_dict, data_key):
